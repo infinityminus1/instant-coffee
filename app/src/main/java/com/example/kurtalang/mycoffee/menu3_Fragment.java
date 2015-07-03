2f2eaea0bf38f13b7781940244d0ce10cacd6281
Kurt Lang <kurtalang@gmail.com>
2015-07-02 22:19:18 -0500

    Intial Commit

---
+++ app/src/main/java/com/example/kurtalang/mycoffee/menu3_Fragment.java
@@ -0,0 +1,23 @@
+package com.example.kurtalang.mycoffee;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kurtalang on 6/22/15.
+ */
+public class menu3_Fragment extends Fragment {
+
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        rootview = inflater.inflate(R.layout.menu3_layout, container, false);
+        return rootview;
+    }
+}
